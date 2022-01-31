e8ccb0bee3ccc520c7c790e887a55390952d9322
Clayton Anderson <Clanderson.Anderson@gmail.com>
2022-01-31 13:34:06 -0800

    Add sounds for Alarm.py to play

--- Alarm.py
+++ Alarm.py
@@ -1,5 +1,9 @@
 from Timer import Timer
+import playsound
 
 class Alarm(Timer):
+
+    startSound = "./Sounds/Start_coding.mp3"
+    stopSound = "./Sounds/Stop_coding.mp3"
     def __init__(self, alarmTime=3600):
         self.alarmTime = alarmTime
